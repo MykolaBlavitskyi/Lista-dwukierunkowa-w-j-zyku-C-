ab9607b7341cd7f3c51b43e477dc983f943cfc93
Blavitskyi <blavitskijmikola@gmail.com>
2024-10-28 11:06:49 +0100

    Dodałem strukture Node

--- Lista dwukierunkowa w języku C++/Lista dwukierunkowa w języku C++.cpp
+++ Lista dwukierunkowa w języku C++/Lista dwukierunkowa w języku C++.cpp
@@ -1,1 +1,12 @@
-﻿
+﻿#include <iostream>
+#include <cstdlib>                                                                                                  //#include <cstdlib> - dla rand() i srand()
+#include <ctime>                                                                                                    //#include <ctime> - dla time()
+using namespace std;
+
+struct Node {                                                                                                       //Node bedzie reprezentować pojedynczy element w liscie dwukierunkowej. Składa się z trzech częsci:
+    int data;                                                                                                       //data - przechowuje wartość 
+    Node* prev;                                                                                                     //prev — wskaznik na poprzedni element listy
+    Node* next;                                                                                                     //next — wskaznik na następny element w liście
+    Node(int value) : data(value), prev(nullptr), next(nullptr) {}                                                  //Node(int value) ustawia wartosc data na value, 
+    //a wskaźniki prev(nullptr) i next(nullptr) na nullptr co oznacza że poczatkowo element nie jest połączony z innymi elementami
+};
